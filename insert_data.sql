30b7a8e965631fa318590e4f23767e77477bd2fe
sven <sven.j.fijan@gmail.com>
2021-11-08 17:36:37 +0100

    added a bit of dummy data

---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+insert into 
+	player(first_name, last_name, current_club, nationality, dob, preffered_pos)
+values
+	('Pero', 'Peric', 'Foo', 'Croatian', '1.1.2001.', 'goalkeeper'),
+	('Ivica', 'Ivankovic', 'Foo', 'German', '2.3.2004.', 'striker'),
+	('John', 'Doe', 'Bar', 'Italian', '5.6.1988.', 'goalkeeper'),
+	('Marko', 'Markovic', 'Bar', 'Croatian', '1.1.2001.', 'center back')
