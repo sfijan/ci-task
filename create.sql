f3ad0de9ef36a19230fd531e06c5ad1bfc3396a7
sven <sven.j.fijan@gmail.com>
2021-11-08 19:13:11 +0100

    added pip requirements file

--- create.sql
+++ create.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS player;
 
+-- table for storing player data
 CREATE TABLE player (
 	player_id SERIAL NOT NULL PRIMARY KEY,
 	first_name TEXT NOT NULL,
@@ -11,6 +12,7 @@ CREATE TABLE player (
 	modified TIMESTAMP DEFAULT NOW()
 );
 
+-- sets the modified field to the current time
 CREATE OR REPLACE FUNCTION trigger_update_modified()
 RETURNS TRIGGER AS
 $$
@@ -20,6 +22,7 @@ BEGIN
 END;
 $$ LANGUAGE PLPGSQL;
 
+-- set a trigger to update the modified field when updated
 CREATE TRIGGER update_modified
 BEFORE UPDATE ON player
 FOR EACH ROW
