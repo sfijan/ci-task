56dd47132839ef276c68f0c99ffadae72a45760e
sven <sven.j.fijan@gmail.com>
2021-11-11 13:28:50 +0100

    reorganized and added full functionality

--- cli/script.py
+++ cli/script.py
@@ -10,53 +10,69 @@ def unix_to_date(ts):
     return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')
 def date_to_unix(s):
     return time.mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f").timetuple())
+def str_to_date(s):
+    return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+
+#write all of the records into a file with the given filename
+def write_to_file(filename, records):
+    if records == []:
+        return
+    if os.path.exists(filename):
+        with open(filename, 'r+') as file:
+            j = json.load(file)                         #get the data,
+            j[str(datetime.datetime.now())] = records   #add the new records
+            file.seek(0, 0)                             #reset and write
+            file.write(json.dumps(j, indent=4, sort_keys=True, default=str))
+
+    else:       #if there is no file, simply write the records
+        with open(filename, 'w') as file:
+            j = {str(datetime.datetime.now()): records}
+            file.write(json.dumps(j, indent=4, sort_keys=True, default=str))
+    return
+
+#return a dictionary with all the player_ids and timestamp of last fetch
+def get_records(filename):
+    if not os.path.exists(filename):
+        return {}
+    ret = {}
+    with open(filename, 'r') as file:
+        j = json.load(file)
+        for ts in j:                #for every fetch time
+            for record in j[ts]:    #for every record
+                ret[record['player_id']] = ts
+    return ret
+
+
 
 parser = argparse.ArgumentParser(description='Get some football players.')
 parser.add_argument('-m', '--method', metavar='METHOD', nargs=1, type=str, default='GET', help='sets the HTTP method to be used (defaults to GET)')
 parser.add_argument('-a', '--address', metavar='url', type=str, help='url address to which to send the request')
 parser.add_argument('-id', metavar='id', nargs='+', type=int, help='select the id or ids of records to get (gets all by default)')
 
+
+single = 'single'
+more = 'more'
 args = parser.parse_args()
-print(args)
+#print(args)
 
-#TODO   add previous records
 if args.method.upper() == 'GET':
-    if args.id and len(args.id) == 1:   #get a single record
-        r = requests.get(args.address + '/' + str(args.id[0]))
-        if os.path.exists('single'):    #if a record exists,
-            with open('single', 'r') as file:
-                saved_id = json.load(file)['player_id'] #and has the same id
-            if (saved_id == args.id[0] and
-                os.path.getmtime('single') > date_to_unix(r.json()['modified'])):   #and is recent enough
-                print('Data is already up to date') #only then is there no need to update
-                exit(0)
+    #fetch records
+    if not args.id:             #no specified ids -> get all
+        filename = more
+        fetched = requests.get(args.address).json()
+    elif len(args.id) == 1:     #single specified id
+        filename = single
+        fetched = [requests.get(args.address + '/' + str(args.id[0])).json()]
+    else:                       #multiple specified ids
+        filename = more
+        fetched = [requests.get(args.address + '/' + str(i)).json() for i in args.id]
 
-        with open('single', 'w') as f:              #otherwise update
-            s = json.dumps(r.json(), indent=4, sort_keys=True, default=str)
-            f.write(s)
-
-    else:       #get multiple records
-        update = False
-        if args.id:     #specified records
-            s = '['
-            for i in args.id:
-                r = requests.get(args.address + '/' + str(i))
-                if not os.path.exists('more') or os.path.getmtime('more') < date_to_unix(r.json()['modified']):
-                    update = True
-                s += json.dumps(r.json(), indent=4, sort_keys=True, default=str) + ','
-            s = s[:-1] + ']'
-        else:           #all the records
-            r = requests.get(args.address)
-            s = '['
-            for i in r.json():
-                s += json.dumps(i, indent=4, sort_keys=True, default=str) + ','
-                if not os.path.exists('more') or os.path.getmtime('more') < date_to_unix(i['modified']):
-                    update = True
-            s = s[:-1] + ']'
-
-        if update:      #update if needed
-            with open('more', 'w') as f:
-                f.write(s)
-        else:
-            print('Data is already up to date')
+    #make a selection and add records
+    stored = get_records(filename)
+    to_put = []
+    for record in fetched:
+        if (not record['player_id'] in stored.keys() or
+                str_to_date(record['modified']) > str_to_date(stored[record['player_id']])):
+            to_put.append(record)
+    write_to_file(filename, to_put)
 
